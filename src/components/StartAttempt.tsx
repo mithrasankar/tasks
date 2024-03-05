e774a26aba12fcff3510a4307f6566874c341839
Mithra <mithra@udel.edu>
2024-03-05 17:40:18 -0500

    all components before twodice and cycleholiday

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,30 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [numAttempts, setNumAttempts] = useState<number>(4);
+    const [inProgress, setInProgress] = useState<boolean>(false);
+    function start(): void {
+        setInProgress(true);
+        setNumAttempts(numAttempts - 1);
+    }
+    function stop(): void {
+        setInProgress(false);
+    }
+    function mulligan(): void {
+        setNumAttempts(numAttempts + 1);
+    }
+    return (
+        <div>
+            <Button onClick={start} disabled={inProgress || numAttempts === 0}>
+                Start Quiz
+            </Button>
+            <Button onClick={stop} disabled={!inProgress}>
+                Stop Quiz
+            </Button>
+            <Button onClick={mulligan} disabled={inProgress}>
+                Mulligan
+            </Button>
+            {numAttempts}
+        </div>
+    );
 }
